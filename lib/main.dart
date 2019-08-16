74788944baa0a13ff20ba7c1d732451cf8c893de
Gerill <gerill@concept-machine.net>
2019-08-16 23:40:06 +0800

    add text

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,76 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+ValueNotifier<double> valueListener = ValueNotifier(.0);
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double txtPos = -4.3;
+
+  void setTxtPos(double num){
+    setState(() {
+      txtPos = num;   
+    });
+  }
+
+  @override
+  void initState() {
+    ValueNotifier<double> v = valueListener;
+
+    v.addListener(() {
+
+      if(v.value * 4.0 > 1.3){
+        setState(() {
+          txtPos = -4.3 + (v.value * 4.0);
+        });
+      }
+     
+
+      print(txtPos);
+      
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    valueListener.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Slider(),
+          child: Container(
+            width: 200,
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(100.0)
+            ),
+            child: Stack(
+              children: <Widget>[
+                Slider(setTxtPos),
+                Align(
+                  alignment: Alignment(txtPos, 0),
+                  child: txtPos < -1.1 ? Text('') :
+                  Text(
+                    'CONFIRM', 
+                    style: TextStyle(
+                      color: Colors.lightBlueAccent, fontSize: 24, fontWeight: FontWeight.w900
+                    )
+                  )
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -16,51 +79,42 @@ class MyApp extends StatelessWidget {
 }
 
 class Slider extends StatefulWidget {
-  final ValueChanged<double> valueChanged;
-
-  Slider({this.valueChanged});
-
+  final Function setTxtPos;
+  Slider(this.setTxtPos);
   @override
-  SliderState createState() {
-    return SliderState();
-  }
+  SliderState createState() => SliderState();
 }
 
 class SliderState extends State<Slider> {
-  ValueNotifier<double> valueListener = ValueNotifier(.0);
-
+  
   @override
   void initState() {
-    valueListener.addListener(notifyParent);
+    valueListener.addListener(() {});
     super.initState();
   }
 
-  void notifyParent() {
-    if (widget.valueChanged != null) {
-      widget.valueChanged(valueListener.value);
-    }
+  @override
+  void dispose() {
+    valueListener.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        color: Colors.green,
-        borderRadius: BorderRadius.circular(100.0)
-      ),
-      height: 50.0,
-      width: 150.0,
       child: Builder(
         builder: (context) {
           final handle = GestureDetector(
-            onHorizontalDragUpdate: (details) {
-              // print(details);
+            onHorizontalDragUpdate: (dtls) {
+              // print('dx ${dtls.delta.dx}');
               valueListener.value = (valueListener.value +
-                      details.delta.dx / context.size.width)
-                  .clamp(.0, 1.0);
+                      dtls.delta.dx / context.size.width).clamp(.0, 1.0);
             },
             onHorizontalDragEnd: (_) {
+              // print('dragEnd ${valueListener.value}');
               if(valueListener.value == 1){
+                widget.setTxtPos(-4.0);
+    
                 showDialog(
                   context: context,
                   builder: (context) {
@@ -72,14 +126,18 @@ class SliderState extends State<Slider> {
               }
               // print(valueListener.value);
               valueListener.value = 0.0;
+              widget.setTxtPos(-4.0);
             },
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.redAccent,
+                color: Colors.black87,
                 borderRadius: BorderRadius.circular(100.0)
               ),
-              height: 50.0,
+              height: 60.0,
               width: 60.0,
+              child: Center(
+                child: Icon(Icons.arrow_forward_ios, color: Colors.white)
+              ),
             ),
           );
 
